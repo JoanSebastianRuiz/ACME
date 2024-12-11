15cf73f79e3c0ba961081ae6e1fadd2c96b45f39
JoanSebastianRuiz <joansebastianruizangarita10@gmail.com>
2024-12-11 16:34:08 -0500

    feat: :bug: ddl and dml database

--- Database/ddl.sql
+++ Database/ddl.sql
@@ -4,39 +4,37 @@ USE acme;
 
 CREATE TABLE Company(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) UNIQUE NOT NULL,
+    name VARCHAR(100) UNIQUE NOT NULL UNIQUE,
     phone CHAR(10) NOT NULL UNIQUE
 );
 
-CREATE TABLE Individual(
+CREATE TABLE AccessSubject(
     id VARCHAR(10) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    role ENUM("worker", "guest") NOT NULL,
-    state ENUM("active", "inactive") NOT NULL,
-    idCompany INT NOT NULL,
     phone CHAR(10) NOT NULL UNIQUE,
     emailAddress VARCHAR(100) NOT NULL UNIQUE,
+    role ENUM("sudo", "manager", "officer", "securityGuard", "worker", "guest") NOT NULL,
+    state ENUM("active", "inactive") NOT NULL,
+    password VARCHAR(100) NULL,
+    idCompany INT NULL,
     FOREIGN KEY(idCompany) REFERENCES Company(id)
 );
 
-CREATE TABLE User(
-    id VARCHAR(10) PRIMARY KEY NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    role ENUM("sudo", "manager", "officer", "securityGuard") NOT NULL,
-    state ENUM("active", "inactive") NOT NULL,
-    idCompany INT NULL,
-    FOREIGN KEY(idCompany) REFERENCES Company(id)
+CREATE TABLE Scanner(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    type ENUM("entry","exit")
 );
 
 CREATE TABLE AccessLog(
     id INT PRIMARY KEY AUTO_INCREMENT,
     type ENUM("entry", "exit") NOT NULL,
     datetime DATETIME NOT NULL,
-    idIndividual VARCHAR(10) NOT NULL,
-    idUser VARCHAR(10) NOT NULL,
-    FOREIGN KEY(idIndividual) REFERENCES Individual(id),
-    FOREIGN KEY(idUser) REFERENCES User(id)
+    idAccessSubject VARCHAR(10) NOT NULL,
+    idScanner INT NULL,
+    idAccessSubjectLogger VARCHAR(10) NULL,
+    FOREIGN KEY(idScanner) REFERENCES Scanner(id),
+    FOREIGN KEY(idAccessSubject) REFERENCES AccessSubject(id),
+    FOREIGN KEY(idAccessSubjectLogger) REFERENCES AccessSubject(id)
 );
 
 CREATE TABLE Annotation(
@@ -44,26 +42,26 @@ CREATE TABLE Annotation(
     datetime DATETIME NOT NULL,
     suspended BOOLEAN NOT NULL, 
     reason TEXT(500) NOT NULL,
-    idIndividual VARCHAR(10) NOT NULL,
-    idUser VARCHAR(10) NOT NULL,
+    idAccessSubject VARCHAR(10) NOT NULL,
+    idAccessSubjectLogger VARCHAR(10) NOT NULL,
     state ENUM("active", "inactive") NOT NULL,
-    FOREIGN KEY(idIndividual) REFERENCES Individual(id),
-    FOREIGN KEY(idUser) REFERENCES User(id)
+    FOREIGN KEY(idAccessSubject) REFERENCES AccessSubject(id),
+    FOREIGN KEY(idAccessSubjectLogger) REFERENCES AccessSubject(id)
 );
 
 CREATE TABLE Vehicle(
     plate VARCHAR(6) PRIMARY KEY,
     type ENUM("car", "motorcycle") NOT NULL,
-    idIndividual VARCHAR(10) NOT NULL,
-    FOREIGN KEY(idIndividual) REFERENCES Individual(id)
+    idAccessSubject VARCHAR(10) NOT NULL,
+    FOREIGN KEY(idAccessSubject) REFERENCES AccessSubject(id)
 );
 
 CREATE TABLE Justification(
     id INT PRIMARY KEY AUTO_INCREMENT,
     datetime DATETIME NOT NULL,
     reason TEXT(500) NOT NULL,
-    idUser VARCHAR(10) NOT NULL,
-    idAnnotation INT NOT NULL,
+    idAccessSubjectLogger VARCHAR(10) NOT NULL,
+    idAnnotation INT NOT NULL UNIQUE,
     FOREIGN KEY(idAnnotation) REFERENCES Annotation(id),
-    FOREIGN KEY(idUser) REFERENCES User(id)
+    FOREIGN KEY(idAccessSubjectLogger) REFERENCES AccessSubject(id)
 );
