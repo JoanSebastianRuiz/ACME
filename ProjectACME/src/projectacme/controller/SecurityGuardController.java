d82616c495227ac40e87d5be13b2dd385a144a68
JuanJTorresB <juanjtorresbecerra@gmail.com>
2024-12-13 02:47:52 -0500

    feat: :construction: SecurityGuardController

--- ProjectACME/src/projectacme/controller/SecurityGuardController.java
+++ ProjectACME/src/projectacme/controller/SecurityGuardController.java
@@ -1,4 +1,45 @@
 package projectacme.controller;
 
+import projectacme.service.SecurityGuard;
+import projectacme.util.Enum.ScannerType;
+
+import java.util.List;
+import java.util.Map;
+
 public class SecurityGuardController {
+    private final SecurityGuard securityGuardService;
+
+    public SecurityGuardController(SecurityGuard securityGuardService) {
+        this.securityGuardService = securityGuardService;
+    }
+
+    public void registerAccess(ScannerType type, String id) {
+        securityGuardService.registerAccess(type, id);
+        System.out.println("Access registered successfully");
+    }
+
+    public void viewUserAnnotation() {
+        securityGuardService.viewUserAnnotation();
+        System.out.println("User annotation viewed");
+    }
+
+    public void registerNotes() {
+        securityGuardService.registerNotes();
+        System.out.println("Notes registered");
+    }
+
+    public void registerPlate() {
+        securityGuardService.registerPlate();
+        System.out.println("Plate registered");
+    }
+
+    public void getReportsUsers() {
+        securityGuardService.getReportsUsers();
+        System.out.println("User reports retrieved");
+    }
+
+    public void getReportsVehicles() {
+        List<Map<String, Object>> reports = securityGuardService.getReportsVehicles();
+        System.out.println("Vehicle reports retrieved: " + reports);
+    }
 }
