ce2e4eaba55c44e6f8e814d0322e2d3ae6dc0c2c
RaitonOwO <jhoanowo@gmail.com>
2024-12-13 16:41:48 -0500

    fix: :bug: 4 imports were eliminated

--- ProjectACME/src/projectacme/controller/LoginController.java
+++ ProjectACME/src/projectacme/controller/LoginController.java
@@ -1,13 +1,50 @@
 package projectacme.controller;
 
+import java.awt.Color;
 import projectacme.model.AccessSubject;
 import projectacme.repository.implementation.AccessSubjectImpl;
-import projectacme.util.Login;
-
-import javax.crypto.spec.PSource;
+import projectacme.view.IntefarceLogin;
 
 public class LoginController {
-    public static boolean validationUser(String document, String password){
-        return Login.validationUser(document, password);
+
+    private IntefarceLogin loginView;
+    private AccessSubjectImpl accessSubjectService;
+
+    public LoginController(IntefarceLogin loginView) {
+        this.loginView = loginView;
+        this.accessSubjectService = new AccessSubjectImpl(); 
+        initController();
+    }
+
+    private void initController() {
+        loginView.getTextButtonEnter().addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                handleLogin();
+            }
+        });
+    }
+
+    private void handleLogin() {
+        String username = loginView.getInputUsername().getText();
+        String password = new String(loginView.getInputPassword().getPassword());
+
+        // Validación de usuario a través del DAO
+        AccessSubject user = accessSubjectService.getAccessSubjectById(username);
+                    System.out.println("A");
+
+
+        if (user != null) {
+            System.out.println("User Validated: " + user.getName());
+            // Abrir nueva ventana
+            IntefarceLogin newWindow = new IntefarceLogin();
+            newWindow.setVisible(true);
+            loginView.dispose(); // Cerrar ventana actual
+        } else {
+            System.out.println("User Is Not Valid");
+            // Mostrar error en campos
+            loginView.getInputUsername().setForeground(Color.RED);
+            loginView.getInputPassword().setForeground(Color.RED);
+        }
     }
 }
