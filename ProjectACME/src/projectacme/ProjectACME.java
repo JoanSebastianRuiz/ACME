fc70f76954b524df086ddecec1552f2deee5a14c
RaitonOwO <jhoanowo@gmail.com>
2024-12-14 00:35:31 -0500

    fix: :sparkles: new interface access database

--- ProjectACME/src/projectacme/ProjectACME.java
+++ ProjectACME/src/projectacme/ProjectACME.java
@@ -31,6 +31,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 import javax.swing.*;
+import projectacme.controller.ManagerMenuController;
 import projectacme.view.InterfaceManagerMenu;
 //import projectacme.view.IntefaceCreateWorker;
 //import projectacme.view.IntefarceLogin;
