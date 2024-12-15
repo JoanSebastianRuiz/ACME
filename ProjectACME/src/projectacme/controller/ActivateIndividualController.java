0e26fb689280c3da6c2b022b45ae4da116e75d65
JoanSebastianRuiz <joansebastianruizangarita10@gmail.com>
2024-12-15 01:43:14 -0500

    feat: :sparkles: Activate Individual Controller

--- ProjectACME/src/projectacme/controller/ActivateIndividualController.java
+++ ProjectACME/src/projectacme/controller/ActivateIndividualController.java
@@ -2,47 +2,47 @@ package projectacme.controller;
 
 import projectacme.model.AccessSubject;
 import projectacme.repository.implementation.AccessSubjectImpl;
-import projectacme.service.Manager;
-import projectacme.service.SecurityGuard;
+import projectacme.service.Individual;
+import projectacme.service.Officer;
 import projectacme.util.Enum.AccessSubjectRoleEnum;
 import projectacme.util.validators.AccessSubjectValidator;
-import projectacme.view.InterfaceActivateGuard;
+import projectacme.view.InterfaceActivateIndividual;
 
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class ActivateGuardController {
-    private final InterfaceActivateGuard view;
-    private Manager manager;
+public class ActivateIndividualController {
+    private final InterfaceActivateIndividual view;
+    private Officer officer;
     private final AccessSubjectImpl accessSubjectImpl = new AccessSubjectImpl();
 
-    public ActivateGuardController(InterfaceActivateGuard view, Manager manager) {
+    public ActivateIndividualController(InterfaceActivateIndividual view, Officer officer) {
         this.view = view;
-        this.manager = manager;
+        this.officer = officer;
 
         view.getTextMessage().setForeground(Color.white);
 
         view.getTextActivate().addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                inactivateSecurityGuard();
+                inactiveIndividual();
             }
         });
     }
 
-    private void inactivateSecurityGuard(){
+    private void inactiveIndividual(){
         String document = view.getinputID().getText();
 
         if(AccessSubjectValidator.accessSubjectValidator(document)){
             AccessSubject accessSubject =accessSubjectImpl.getAccessSubjectById(document);
-            if(accessSubject.getRole()== AccessSubjectRoleEnum.securityGuard){
-                SecurityGuard securityGuard = (SecurityGuard) accessSubject;
-                boolean result = manager.activateSecurityGuard(securityGuard);
+            if(accessSubject.getRole()== AccessSubjectRoleEnum.worker || accessSubject.getRole()== AccessSubjectRoleEnum.guest){
+                Individual individual = (Individual) accessSubject;
+                boolean result = officer.activateIndividual(individual);
 
                 if(result){
                     view.getinputID().setText("");
-                    view.getTextMessage().setText("Security guard activated");
+                    view.getTextMessage().setText("Person activated");
                     view.getTextMessage().setForeground(Color.GREEN);
                 } else{
                     view.getTextMessage().setText("Invalid ID");
