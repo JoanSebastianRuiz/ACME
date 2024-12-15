e8cfbd45d3e88fd3f008df736565b493e6dd27f1
JuanJTorresB <juanjtorresbecerra@gmail.com>
2024-12-14 22:18:32 -0500

    feat: :sparkles: UserActual Static Added

---
+++ ProjectACME/src/projectacme/UserActual.java
@@ -0,0 +1,31 @@
+package projectacme;
+
+import projectacme.model.AccessSubject;
+import projectacme.service.Manager;
+import projectacme.service.Officer;
+import projectacme.service.SecurityGuard;
+import projectacme.service.Sudo;
+
+public class UserActual {
+    private static AccessSubject accessSubject;
+
+    public static Sudo getAccessSubjectSudo() {
+        return (Sudo) accessSubject;
+    }
+
+    public static Manager getAccessSubjectManager() {
+        return (Manager) accessSubject;
+    }
+
+    public static Officer getAccessSubjectOfficer() {
+        return (Officer) accessSubject;
+    }
+
+    public static SecurityGuard getAccessSubjectSecurityGuard() {
+        return (SecurityGuard) accessSubject;
+    }
+
+    public static void setAccessSubject(AccessSubject accessSubject) {
+        UserActual.accessSubject = accessSubject;
+    }
+}
